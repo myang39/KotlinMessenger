ea9fe24789c5b80e565d1581bf69c9453958fe2a
Minkang Yang <lukeyang008@gmail.com>
2020-06-13 15:45:30 -0400

    intent to Login and back

--- app/src/main/java/lukeyang/com/LoginActivity.kt
+++ app/src/main/java/lukeyang/com/LoginActivity.kt
@@ -1,14 +1,26 @@
 package lukeyang.com
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        login_button_login.setOnClickListener {
+            val email = email_edittext_login.text.toString()
+            val password = password_edittext_login.text.toString()
 
+            Log.d("LoginActivity", "email is $email")
+            Log.d("LoginActivity", "password is $password")
+        }
+
+        back_to_login_textview.setOnClickListener {
+            finish()
+        }
     }
 
 }
